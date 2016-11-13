c55af0e2e299073078025142c57c1a2fdf621305
xyryx <ajeyatj@Ajeyas-MacBook-Pro.local>
2016-11-12 23:30:56 -0500

    added accuracies for advanced tokenizer

--- src/Main.py
+++ src/Main.py
@@ -170,13 +170,13 @@ alphas = [1, 5, 10, 15, 20, 25, 30, 35]
 
 tokenizer = AdvancedTokenizer()
 # reviewPolarityAccuracies = evaluateReviewPolarity(K, tokenizer, alphas)
-imdbAccuracies = evaluateIMDB(K, tokenizer, alphas)
+# imdbAccuracies = evaluateIMDB(K, tokenizer, alphas)
 # subjectivityAccuracies = evaluateSubjectivity(K, tokenizer, alphas)
 
-# reviewPolarityAccuracies = [0.8025, 0.8225, 0.83, 0.835, 0.8425, 0.8375, 0.8275, 0.8075]
-# imdbAccuracies = [0.82312, 0.83088, 0.83392, 0.83532, 0.83532, 0.83576, 0.83628, 0.83652]
-# subjectivityAccuracies = [0.8961038961038961, 0.9050949050949051, 0.9050949050949051, 0.9015984015984015, 0.8986013986013986, 0.8966033966033966, 0.8946053946053946, 0.8931068931068931]
-# printTable("Advanced Tokenizer", alphas, reviewPolarityAccuracies, imdbAccuracies)
+reviewPolarityAccuracies = [0.8025, 0.8225, 0.83, 0.835, 0.8425, 0.8375, 0.8275, 0.8075]
+imdbAccuracies = [0.83256, 0.84004, 0.844, 0.84448, 0.84556, 0.84692, 0.84772, 0.84792]
+subjectivityAccuracies = [0.8961038961038961, 0.9050949050949051, 0.9050949050949051, 0.9015984015984015, 0.8986013986013986, 0.8966033966033966, 0.8946053946053946, 0.8931068931068931]
+printTable("Advanced Tokenizer", alphas, reviewPolarityAccuracies, imdbAccuracies, subjectivityAccuracies)
 
 # tokenizer = BigramTokenizer()
 # reviewPolarityAccuracies = evaluateReviewPolarity(K, tokenizer, alphas)
@@ -185,5 +185,3 @@ imdbAccuracies = evaluateIMDB(K, tokenizer, alphas)
 # reviewPolarityAccuracies = [0.805, 0.8275, 0.8275, 0.83, 0.8425, 0.8325, 0.8425, 0.8275]
 # imdbAccuracies = [0.83364, 0.84168, 0.8436, 0.84436, 0.84496, 0.84532, 0.84548, 0.84592]
 # printTable("Bigram Tokenizer", alphas, reviewPolarityAccuracies, imdbAccuracies)
-
-print(imdbAccuracies)
